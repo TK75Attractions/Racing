3240db5646a6730e34430b388d575f2bc0254a83
the-fur-lover-sketch <ykj43313@gmail.com>
2026-05-06 13:16:35 +0900

    a

--- Assets/Managers/InputManager.cs
+++ Assets/Managers/InputManager.cs
@@ -42,8 +42,8 @@ public class InputManager : MonoBehaviour
             //if (peddale < 0) peddale = 0;
 
             handle = 0;
-            if (Keyboard.current.dKey.isPressed) handle = 30;
-            if (Keyboard.current.aKey.isPressed) handle = -30;
+            if (Keyboard.current.dKey.isPressed) handle = 30f;
+            if (Keyboard.current.aKey.isPressed) handle = -30f;
         }
     }
 }
