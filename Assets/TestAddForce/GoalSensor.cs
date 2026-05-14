4cba54d15db72eb66fd39103935d1e90905e598f
the-fur-lover-sketch <ykj43313@gmail.com>
2026-05-14 12:51:43 +0900

    タイム計測

--- Assets/TestAddForce/GoalSensor.cs
+++ Assets/TestAddForce/GoalSensor.cs
@@ -2,14 +2,16 @@ using UnityEngine;
 
 public class GoalSensor : MonoBehaviour
 {
+    [SerializeField] private LapManager lapManager;
+
+    
     public int lapCount = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void OnTriggerEnter(Collider other)
     {
-        if (other.attachedRigidbody != null && other.attachedRigidbody.CompareTag("Player"))
-        {
-            lapCount++;
-            Debug.Log($"Lap {lapCount} passed");
-        }
+        
+        lapCount++;
+        Debug.Log($"Lap {lapCount} passed");
+        lapManager.OnCarPassGoal(other.attachedRigidbody);
     }
 }
